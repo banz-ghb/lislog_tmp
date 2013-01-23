179698b55a618e0869e64a206744fd3fd609e575
banz-ghb <banz.ggl@gmail.com>
2013-01-24 00:09:44 +0900

    add namespace for action type

--- index.php
+++ index.php
@@ -408,7 +408,7 @@ This is your app
     ?>
 <section id="activity" class="clearfix">
 <div class="fb-activity" data-app-id="554694347877002" data-width="300" data-height="300" data-header="true" data-recommendations="false"></div>’
-<div class="fb-activity" data-site="https://lislog.herokuapp.com/radio/jp/co/tbs/fumou.html" data-app-id="554694347877002" data-action="tune_in" data-width="300" data-height="300" data-header="true" data-recommendations="false"></div>
+<div class="fb-activity" data-site="https://lislog.herokuapp.com/radio/jp/co/tbs/fumou.html" data-app-id="554694347877002" data-action="lislogapp:tune_in" data-width="300" data-height="300" data-header="true" data-recommendations="false"></div>
 </section>
 <section id="guides" class="clearfix">
 <h1>Learn More About Heroku &amp; Facebook Apps</h1>
