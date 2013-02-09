d824a772a97f5c2290b2c7fc59daf12b0a24c5f3
banz-ghb <banz.ggl@gmail.com>
2013-02-09 11:24:43 +0900

    remove no need buttons

--- index.php
+++ index.php
@@ -293,33 +293,18 @@ This is your app
 
 <div id="share-app">
 <p>Share lislog:</p>
-<ul>
-<li>
-<a href="#" class="facebook-button" id="publishAction_fumou" data-url="<?php echo AppInfo::getUrl(); ?>">
-<span class="plus">Fumou</span>
-</a>
-</li>
-<li>
-<a href="#" class="facebook-button" id="publishAction" data-url="<?php echo AppInfo::getUrl(); ?>">
-<span class="plus">Publish Action</span>
-</a>
-</li>
-<li>
-<a href="#" class="facebook-button" id="postToWall" data-url="<?php echo AppInfo::getUrl(); ?>">
-<span class="plus">Post to Wall</span>
-</a>
-</li>
-<li>
-<a href="#" class="facebook-button speech-bubble" id="sendToFriends" data-url="<?php echo AppInfo::getUrl(); ?>">
-<span class="speech-bubble">Send Message</span>
-</a>
-</li>
-<li>
-<a href="#" class="facebook-button apprequests" id="sendRequest" data-message="Test this awesome app">
-<span class="apprequests">Send Requests</span>
-</a>
-</li>
-</ul>
+  <ul>
+    <li>
+      <a href="#" class="facebook-button" id="publishAction_fumou" data-url="<?php echo AppInfo::getUrl(); ?>">
+        <span class="plus">Fumou</span>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="facebook-button" id="publishAction" data-url="<?php echo AppInfo::getUrl(); ?>">
+        <span class="plus">Publish Action</span>
+      </a>
+    </li>
+  </ul>
 </div>
 </div>
 <?php } else { ?>
