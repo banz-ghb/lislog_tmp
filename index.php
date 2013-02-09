bd2dfd27c67161f6b1fe0d1e3b1a236d88712d73
banz-ghb <banz.ggl@gmail.com>
2013-02-09 09:30:27 +0900

    modify comment

--- index.php
+++ index.php
@@ -247,8 +247,6 @@ window.fbAsyncInit = function() {
  var js, fjs = d.getElementsByTagName(s)[0];
  if (d.getElementById(id)) return;
  js = d.createElement(s); js.id = id;
- //2013-02-06 banz-ghb start i18n
- //js.src = "//connect.facebook.net/<?php echo he(idx($basic, 'locale')); ?>/all.js";
  js.src = "//connect.facebook.net/en_US/all.js";
  //2013-02-06 banz-ghb end   i18n
  fjs.parentNode.insertBefore(js, fjs);
